a032652d0e23f94f1e2f3b01c93d2b987ba3ac07
Antheriox <proktgaming.gamez@gmail.com>
2022-11-24 14:40:53 +0100

    Coins scoreboard
    Hoeveelheid coins wordt weergegeven

--- ITItsinTheGame/Assets/Scripts/Backpack.cs
+++ ITItsinTheGame/Assets/Scripts/Backpack.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Backpack : MonoBehaviour
 {
-    public Text ScoreText;
+    public TMP_Text ScoreText;
     public int Coins;
     
     void Start()
@@ -13,13 +14,14 @@ public class Backpack : MonoBehaviour
         Coins = 0;
         ScoreText.text = "Coins: " + Coins;
     }
-    }
+    
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Coin")
         {
-             Coins +=1;
+             Coins = Coins + 1;
             ScoreText.text = "Coins: " + Coins;
       }
     }
+}
     
--- ITItsinTheGame/Assets/Scripts/CoinCollection.cs
+++ ITItsinTheGame/Assets/Scripts/CoinCollection.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CoinCollection : MonoBehaviour
 {
+    public AudioSource AS;
  void OnTriggerEnter2D(Collider2D collision)
  {
     if (collision.gameObject.tag == "Player")
     {
+        AS = GetComponent<AudioSource>();
+        AS.Play();
         Destroy(gameObject);
-        
     }
  }
 }
