d8ea32bc4915c9e749e00af141c3bf74b16b1105
Antheriox <proktgaming.gamez@gmail.com>
2022-11-24 12:45:27 +0100

    Help

--- ITItsinTheGame/Assets/Scripts/Backpack.cs
+++ ITItsinTheGame/Assets/Scripts/Backpack.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Backpack : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text ScoreText;
+    public int Coins;
     void Start()
     {
-        
+        ScoreText.text = "Coins: " + Coins;
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "Coin")
+        {
+            Coins = Coins +1;
+      }
     }
 }
